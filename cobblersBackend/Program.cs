83067667b62fb864ab29d30ec7e1718ea9b9957c
Ymir Arnarson <ymar@itu.dk>
2026-06-19 12:10:27 +0100

    add(program.cs): added DI for SignalR

--- cobblersBackend/Program.cs
+++ cobblersBackend/Program.cs
@@ -1,3 +1,4 @@
+using cobblersBackend.Hubs;
 using cobblersBackend.Services;
 
 
@@ -9,13 +10,21 @@ builder.Services.AddHttpClient<IPistonClient, PistonClient>(client =>
 {
     client.BaseAddress = new Uri(builder.Configuration["Piston:BaseUrl"] ?? "http://localhost:2000/");
 });
+
+// for existing REST api.
 builder.Services.AddControllers();
+
+// for websocket connection with SignalR 
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
 app.MapControllers();
+
+//configure websocket.
+app.MapHub<ExecutorHub>("/hubs/executor");
 
 // run
 app.Run();
