69f5399644a05039cc4dce8f30e8cc4074922f14
Ymir Arnarson <ymar@itu.dk>
2026-06-30 13:33:16 +0100

    fix(DTOs): perameter spelling mistakes

--- cobblersBackend/DTOs/ExecuteResponseDto.cs
+++ cobblersBackend/DTOs/ExecuteResponseDto.cs
@@ -1,7 +1,7 @@
 
 namespace cobblersBackend.DTOs;
 
-public record ExecuteResponseDto(string Status, string Stout, string Sterr);
+public record ExecuteResponseDto(string Status, string Stdout, string Stderr);
 
 public static class ExecuteStatus
 {
