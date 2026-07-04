e7626376db0409d39fb564cc7a771af7b74e076b
alimacunha <amanda.lima.cunha@alumni.usp.br>
2026-07-04 19:51:10 +0200

    add OpenAPI support for development environment in Program.cs and update README

--- cobblersBackend/Program.cs
+++ cobblersBackend/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddHttpClient<IPistonClient, PistonClient>(client =>
 {
     client.BaseAddress = new Uri(builder.Configuration["Piston:BaseUrl"] ?? "http://localhost:2000/");
 });
+builder.Services.AddOpenApi();
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
@@ -19,6 +20,11 @@ builder.Services.AddControllers()
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
 app.UseHttpsRedirection();
 app.MapControllers();
 
