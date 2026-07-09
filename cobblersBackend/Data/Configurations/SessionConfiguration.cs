3d619c0601d5dbc581d1e4d2de2c452751fea9b2
Ymir Arnarson <ymar@itu.dk>
2026-07-09 14:58:56 +0100

    Created(Configurations): session and student flutter Configurations for database

---
+++ cobblersBackend/Data/Configurations/SessionConfiguration.cs
@@ -0,0 +1,26 @@
+using cobblersBackend.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace cobblersBackend.Data.Configurations;
+
+public class SessionConfiguration : IEntityTypeConfiguration<Session>
+{
+    public void Configure(EntityTypeBuilder<Session> builder)
+    {
+        builder.HasKey(s => s.SessionId);
+        builder.Property(s => s.SessionId)
+               .ValueGeneratedNever();
+               
+        builder.Property(s => s.CreateDateTime)
+               .HasColumnName("timestamptz");
+        
+        builder.HasIndex(s => new { s.Code, s.Year})
+               .IsUnique();
+
+        builder.HasOne(s => s.TaskSet)
+               .WithMany(ts => ts.Sessions)
+               .HasForeignKey(s => s.TaskSetId)
+               .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ cobblersBackend/Data/Configurations/StudentConfiguration.cs
@@ -0,0 +1,15 @@
+using cobblersBackend.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace cobblersBackend.Data.Configurations;
+
+public class StudentConfiguration : IEntityTypeConfiguration<Student>
+{
+    public void Configure(EntityTypeBuilder<Student> builder)
+    {
+        builder.HasKey(s => s.Id);
+        builder.Property(s => s.Id)
+               .ValueGeneratedNever();
+    }
+}
